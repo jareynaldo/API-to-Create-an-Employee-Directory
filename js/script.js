05c8c901a376948d9c3baf8786725db07c6f4a13
José Reynaldo <ja.reynaldo@outlook.com>
2022-04-10 15:22:55 -0400

    works, but only by checking breakpoints

--- js/script.js
+++ js/script.js
@@ -13,10 +13,9 @@ function fetchData(url) {
      fetch(url)
              .then(checkStatus)  
              .then(res => res.json())
-             .then(url => filterThrough(url))
+             .then(url => url.results)
              .then(data => postData(data))
-             .catch(error => console.log('Looks like there was a problem!', error));   
-    setTheTime();
+             .catch(error => console.log('Looks like there was a problem!', error));
 }
 
 // helper functions 
@@ -26,123 +25,77 @@ if (response.ok) {
 } else {
     return Promise.reject(new Error(response.statusText));
 }
-}
 
-function filterThrough(url){
-     return url.results;
 }
 
 // submit abreviation;
 
-function image (data){ return data.picture.large;}
 function fullName (data){ return data.name.first + ' ' + data.name.last;}
-function email (data){ return data.email;}
-function city (data){return data.location.city;}
-function phone (data){
-    let first3 = data.cell.slice(0,3);
-    let lastOnes = data.cell.slice(5,14); 
-    return `(` + first3 + ')' + lastOnes;
-}
-
 function adress (data){return data.location.street.number + ' ' + data.location.street.name + ', ' + data.location.state + ' ' + data.location.postcode;}
-function birthday (data){ return data.dob.date.slice(2,10)}
-
-function reverseDate (date){
-    let splitDate = date.split("-").reverse();
+function birthday (data){
+    let original = data.dob.date.slice(2,10)
+    let splitDate = original.split("-").reverse();
     return splitDate.join("-");
 }
 
 // submit data to html 
 function postData (data){
-    let posted = data.reduce((total, element)  => {
-         total += `
+    let posted = data.map( (employee) => ( `
         <div class="employee">
-        <img src='${image(element)}'data-caption="${fullName(element)}" alt="one of our loveley employess">
-        <div class="employee-text">
-        <h3 class="name" >${fullName(element)}</h3>
-        <p>${email(element)}</p>
-        <p>${city(element)}</p>
-        </div>
+            <img src='${employee.picture.large}'data-caption="${fullName(employee)}" alt="one of our loveley employess">
+            <div class="employee-text">
+                <h3 class="name" >${fullName(employee)}</h3>
+                <p>${employee.email}</p>
+                <p>${employee.location.city}</p>
+            </div>
         </div>
         <div id="myModal" class="modal">
             <div id="myclass">
-            <div class="back">&#x2190</div>
-            <span class="close">&times;</span>
-                <img class="popupImg" src='${image(element)}' alt="one of our loveley employess">
+                <div class="back">&#x2190</div>
+                <span class="close">&times;</span>
+                <img class="popupImg" src='${employee.picture.large}' alt="one of our loveley employess">
                 <div class="m">
-                    <h3>${fullName(element)}</h3>
-                    <p>${email(element)}</p>
-                    <p>${city(element)}</p>
+                    <h3>${fullName(employee)}</h3>
+                    <p>${employee.email}</p>
+                    <p>${employee.location.city}</p>
                 </div>
                 <div class="border"></div>
                 <div class="s">
-                    <p>${phone(element)}</p>
-                    <p>${adress(element)}</p>
-                    <p>Birthday: ${reverseDate(birthday(element))}</p>
+                    <p>${employee.cell}</p>
+                    <p>${adress(employee)}</p>
+                    <p>Birthday: ${birthday(employee)}</p>
                 </div>
                 <div class="next">&#x2192;</div>
             </div>
         </div>
-        `;
-        return total;
-    },[]);  
-
-    employees.innerHTML = posted;    
+        `
+    ));  
+    employees.innerHTML = posted.join("");    
 }
 
 
 // calling function
-let users = fetchData('https://randomuser.me/api/?results=12');
+ fetchData('https://randomuser.me/api/?results=12');
 
 //event listeners
 
-function setTheTime(){
-    setTimeout(atLast, 250);
-}   
-
-function nextElement (elements){
-    [...elements].forEach(element => {
-        element.addEventListener("click", () =>{
-            element
-                .parentElement
-                .parentElement
-                .style.display = "none";
-
-            element
-                .parentElement
-                .parentElement
-                .nextElementSibling
-                .nextElementSibling
-                .style.display ="block";
-        });
-    });
-}
-function previous (elements){
-    [...elements].forEach(element => {
-        element.addEventListener("click", () =>{
-            element
-                .parentElement
-                .parentElement
-                .style.display = "none";
-                
-            let lastElement = element.parentElement.parentElement;
-            lastElement.previousElementSibling.previousElementSibling.style.display ="block";
-        });
-    });
-}
-
-function atLast(){
-    const employee = document.getElementsByClassName('employee');
+window.onload = function atLast(){
+    const employee = document.querySelectorAll('.employee');
     const button = document.getElementsByClassName(`close`);
     const name = document.getElementsByClassName(`name`);
     const next = document.getElementsByClassName(`next`);
     const back = document.getElementsByClassName(`back`);
     // listeners to open and close elements
-    [...employee].forEach(emplo => {
-        emplo.addEventListener('click', () =>{
-            emplo.nextElementSibling.style.display ="block";
+    console.log(employee);
+    for( let i = 0; i < employee.length; i++){
+        employee[i].addEventListener('click', () =>{
+            console.log('clicked');
+            employee[i].nextElementSibling.style.display ="block";
         });
-    });
+    }
+    // employee.forEach(emplo => {
+        
+    // });
     [...button].forEach(btn => {
         btn.addEventListener('click', () => {
             btn.parentElement.parentElement.style.display = "none";
@@ -162,7 +115,31 @@ function atLast(){
            });
     });
   // listener to toggle back and forth
-  
-    previous(back);
-    nextElement(next);
+    [...next].forEach(nexty => {
+        nexty.addEventListener("click", () =>{
+            nexty
+                .parentElement
+                .parentElement
+                .style.display = "none";
+
+            nexty
+                .parentElement
+                .parentElement
+                .nextElementSibling
+                .nextElementSibling
+                .style.display ="block";
+        });
+    });
+
+    [...back].forEach(backy => {
+        backy.addEventListener("click", () =>{
+            backy
+                .parentElement
+                .parentElement
+                .style.display = "none";
+                
+            let lastElement = backy.parentElement.parentElement;
+            lastElement.previousElementSibling.previousElementSibling.style.display ="block";
+        });
+    });
 }
